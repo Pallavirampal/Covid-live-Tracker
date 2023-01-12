bdde56d254bd2fc4b28b5d7282d3e366510fd847
Pallavi rampal <pallavirampal96@gmail.com>
2023-01-12 17:38:40 +0530

    css-changed

--- livecovidtracker/src/components/covid.jsx
+++ livecovidtracker/src/components/covid.jsx
@@ -1,43 +1,58 @@
-import React, { useEffect } from 'react'
+import React, { useEffect,useState } from 'react'
 import './covid.css';
 const Covid = () => {
+
+    const [data,setData] = useState([]);
+
     const getCovidData = async () => {
         try {
-            const res = await fetch('https://api.rootnet.in/covid19-in/stats/history');
+            const res = await fetch('https://api.rootnet.in/covid19-in/stats/latest');
             const actualData = await res.json();
-            console.log(actualData.data[0]);
+            console.log(actualData.regional[0]);
+            setData(actualData.regional[0]);
         } catch (err) {
             console.log(err);
         }
     }
     useEffect(() => {
-        // getCovidData();
-    }, [])
+        getCovidData();
+    }, []);
 
     return (
         <>
-            <section>
-                <h1>Live</h1>
-                <h2>Covid-19 Coronavirus Tracker</h2>
+            <div className="container">
                 <div className="card_main">
-                    <div className='card_inner'>
-                        <p className='card_name'><span> Our </span> Country </p>
-                        <p className='card_total card_small'>INDIA</p>
-                    </div>
-                    <div className='card_inner'>
-                        <p className='card_name'><span> Our </span> Country </p>
-                        <p className='card_total card_small'>INDIA</p>
-                    </div>
-                    <div className='card_inner'>
-                        <p className='card_name'><span> Our </span> Country </p>
-                        <p className='card_total card_small'>INDIA</p>
-                    </div>
-                    <div className='card_inner'>
-                        <p className='card_name'><span> Our </span> Country </p>
-                        <p className='card_total card_small'>INDIA</p>
+                    <h1>LIVE</h1>
+                    <h2>COVID-LIVE-TRACKER</h2>
+                    <div className="card_container">
+                        <div className="card_inner">
+                            <p className='card_name'><span>our </span>COUNTRY</p>
+                            <p className="card_text">INDIA</p>
+                        </div>
+                        <div className="card_inner1">
+                            <p className='card_name'><span>Total </span>RECOVERED</p>
+                            <p className="card_text">{data.discharged}</p>
+                        </div>
+                        <div className="card_inner2">
+                            <p className='card_name'><span>Total </span>CONFIRMED</p>
+                            <p className="card_text">INDIA</p>
+                        </div>
+                        <div className="card_inner3">
+                            <p className='card_name'><span>Total </span>DEATH</p>
+                            <p className="card_text">INDIA</p>
+                        </div>
+                        <div className="card_inner4">
+                            <p className='card_name'><span>Total </span>ACTIVE</p>
+                            <p className="card_text">INDIA</p>
+                        </div>
+                        <div className="card_inner5">
+                            <p className='card_name'><span>Last </span>UPDATED</p>
+                            <p className="card_text">INDIA</p>
+                        </div>
                     </div>
                 </div>
-            </section>
+
+            </div>
 
         </>
     )
